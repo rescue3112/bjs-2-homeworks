8fc3f7c35b1c7fde3c4ce2a283cb9755f2d20fee
rescue3112 <spasatel.3112@gmail.com>
2021-11-10 19:02:57 +0700

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,23 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr = [];
+    const discriminant = b ** 2 - 4 * a * c;;
+    if (discriminant > 0) {
+        arr[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+        arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
+    } else if (discriminant == 0) {
+        arr[0] = -b / (2 * a);
+    } else if (discriminant < 0) {
+        arr;
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    let totalAmount;
 
-  // код для задачи №2 писать здесь
+    // код для задачи №2 писать здесь
 
-  return totalAmount;
+    return totalAmount;
 }
