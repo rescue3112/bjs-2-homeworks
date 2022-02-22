5cd0b102935e6049538bbc8ffa02eeaabefeec6c
rescue3112 <spasatel.3112@gmail.com>
2022-02-22 21:33:03 +0700

    Это решение задачи к 2 лекции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,47 @@
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+let sum = 0;
+let avg = 0;
+let min = arr[0];
+let max = arr[0];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+    sum += arr[i];
 }
-
+avg = (sum / arr.length).toFixed(2);
+avg = Number(avg);
+return {
+    min: min,
+    max: max,
+    avg: avg
+};
+}
 // Задание 2
-function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+function worker([...arr]) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+    let max = func(arrOfArr[0]);
+    for (let i = 0; i < arrOfArr.length; i++) {
+        let resultOfFunc = func(arrOfArr[i]);
+        if (resultOfFunc > max) {
+            max = resultOfFunc;
+        }
+    }
+    return max;
 }
 
 // Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+function worker2([...arr]) {
+    let arrParams = getArrayParams(arr);
+    return Math.abs(arrParams.max - arrParams.min);
+};
