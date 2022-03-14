dbe910d49f7731b4142621b3764dc8dd17fad210
rescue3112 <spasatel.3112@gmail.com>
2022-03-14 22:23:40 +0700

    Доработка массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,27 +1,9 @@
 function compareArrays(arr1, arr2) {
-    let result;
-
-    // Ваш код
-    if (arr1.length != arr2.length) {
-        return false;
-    }
-
-    arr1.filter(function(cv, index, arr) {
-        result = cv == arr2[index];
-        return result;
-    });
-
-    return result; // boolean
+    return (arr1.length == arr2.length) && arr1.every((item, index) => item === arr2[index]);
 }
 
+
+
 function advancedFilter(arr) {
-    let resultArr;
-
-    resultArr = arr.filter(function(cv, idx, arr) {
-        return cv > 0 && cv % 3 == 0;
-    }).map(function(cv, idx, arr) {
-        return cv * 10;
-    });
-
-    return resultArr; // array
+    return arr.filter((item) => item > 0 && !(item % 3)).map((item) => item * 10);
 }
