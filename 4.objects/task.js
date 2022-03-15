60f8e0ed829fb83a4b05783f861a2720152b711e
rescue3112 <spasatel.3112@gmail.com>
2022-03-15 23:14:25 +0700

    Решение задачи к лекции 4 обьекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...mark) {
+    if (this.marks === undefined) {
+        this.marks = [...mark];
+    } else {
+        this.marks.push(...mark);
+    }
+}
+
+Student.prototype.getAverage = function() {
+    const sum = this.marks.reduce((acc, number) => acc + number);
+    return (sum / this.marks.length);
+}
+
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
