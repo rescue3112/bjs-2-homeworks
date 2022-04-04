3f5f72f3cd9298fca793d97448d1b5444e44953d
rescue3112 <spasatel.3112@gmail.com>
2022-04-04 22:45:34 +0700

    Задача декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,69 @@
+const hash = (args) => args.join(',');
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  function wrapper(...rest) {
+    let key = hash(rest);
+    if (key in cache) {
+      console.log("Из кэша: " + cache[key]);
+      return "Из кэша: " + cache[key];
+    } 
+    if (Object.keys(cache).lenght > 5) {
+      delete cache[Object.keys(cache)[0]];
+    }
+    let result = func(...rest);
+    cache[key] = result;
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
-
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, timeout) {
+  let lastCallArgs;
+  let timer;
+  let flag = false;
+  function wrapper(...rest) {
+    lastCallArgs = rest;
+    if (!flag) {
+      flag = true;
+      timer = setTimeout(() => {
+        func(...lastCallArgs);
+        flag = false;
+      }, timeout);
+      return func(...rest);
+    } else {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...lastCallArgs);
+        flag = false;
+      }, timeout);
+    }
+  }
+  return wrapper;
 }
-
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, timeout) {
+  let lastCallArgs;
+  let timer;
+  let flag = false;
+  wrapper.count = 0;
+  function wrapper(...rest) {
+    lastCallArgs = rest;
+    if (!flag) {
+      flag = true;
+      timer = setTimeout(() => {
+      	console.log('Кол-во вызовов: ' + ++wrapper.count);
+        func(...lastCallArgs);
+        flag = false;
+      }, timeout);
+      console.log('Кол-во вызовов: ' + ++wrapper.count);
+      return func(...rest);
+    } else {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+      	console.log('Кол-во вызовов: ' + ++wrapper.count);
+        func(...lastCallArgs);
+        flag = false;
+      }, timeout);
+    }
+  }
+  return wrapper;
 }
